a97f044e13db414ef82b7324f4ac67800b5a2cc8
ncip48 <mbahcip00@gmail.com>
2024-03-17 12:44:32 +0700

    feat: controller policy

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PolicyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +19,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::group(['domain' => '{account}.policy.dotech.cfd'], function () {
-    Route::get('/', function ($account) {
-        return $account;
-    });
+    Route::get('/', [PolicyController::class, 'index']);
 });
