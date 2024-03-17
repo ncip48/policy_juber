4c88760425896f17b9cc7d8bec1fd52cd608d5e9
ncip48 <mbahcip00@gmail.com>
2024-03-17 14:12:02 +0700

    r

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('admin', AdminController::class);
 
 Route::group(['domain' => '{account}.policy.dotech.cfd'], function () {
     Route::get('/', [PolicyController::class, 'index']);
