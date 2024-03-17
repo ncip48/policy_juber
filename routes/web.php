e403bd3386d625e84022dca4dd97325a35c1cda8
ncip48 <mbahcip00@gmail.com>
2024-03-17 15:00:03 +0700

    color

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PolicyController::class, 'index']);
+// Route::get('/', [PolicyController::class, 'index']);
 
 Route::group(['domain' => '{account}.policy.dotech.cfd'], function () {
     Route::get('/', [PolicyController::class, 'index']);
